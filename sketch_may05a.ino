4131607853ab618251d6dcd3db64a3adc37c9630
Tim2244 <timontunes@gmail.com>
2021-05-07 03:44:32 +0530

    Angle Method Update
    ◘ Changed angle measurement using acc gravity vector for orientation instead of fusion algo. -->very stable and nice 
    ◘ There is no need to map, but rather to have a sensitivity multiplication factor. 
    ◘ Have to figure out a way to automatically calibrate sensitivity factor.

--- sketch_may05a.ino
+++ sketch_may05a.ino
@@ -1,86 +1,61 @@
 #include "Nano33BleHID.h"
 #include <Arduino_LSM9DS1.h>
 #include "MadgwickAHRS.h"
+#include "SensorFusion.h" 
 #define M_PI 3.141592653589793238462643
 
-// initialize a Madgwick filter:
-Madgwick filter;
-// sensor's sample rate is fixed at 104 Hz:
-const float sensorRate = 104.00;
-
+float xAcc, yAcc, zAcc;
 float yaw, pitch, roll;
-float vertZero, horzZero;
 float vertValue, horzValue;
-float prev_horzValue, prev_vertValue;
 const float sensitivity =0.001;
+const float miu = 0.001;
+int sign = 1;
 
-// Alias to Nano33BleHID<HIDGamepadService>
 Nano33BleMouse mouse("Ble Mouse");
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
+  
   // attempt to start the IMU:
   if (!IMU.begin()) {
     Serial.println("Failed to initialize IMU");
     // stop here if you can't access the IMU:
     while (true);
   }
-  // start the filter to run at the sample rate:
-  filter.begin(sensorRate);
-  
+
     // initialize the ble HID.
     mouse.initialize();
-
+    
     // Launch the eventqueue thread.
     MbedBleHID_RunEventThread();
-    yaw = 0.0;
-    pitch = 0.0;
-    roll = 0.0;
-    vertZero = 0;
-    horzZero = 0;
-    prev_horzValue = 0;
-    prev_vertValue = 0;
+
 }
 
 void loop() {
   // values for acceleration and rotation:
-  float xAcc, yAcc, zAcc;
-  float xGyro, yGyro, zGyro;
-
-  // values for orientation:
-  float roll, pitch, heading;
-  // check if the IMU is ready to read:
   if (IMU.accelerationAvailable() &&
       IMU.gyroscopeAvailable()) {
     // read accelerometer &and gyrometer:
-    IMU.readAcceleration(xAcc, yAcc, zAcc);
-    IMU.readGyroscope(xGyro, yGyro, zGyro);
+    IMU.readAcceleration(xAcc, yAcc, zAcc);}
 
-    // update the filter, which computes orientation:
-    filter.updateIMU(xGyro, yGyro, zGyro, xAcc, yAcc, zAcc);
+    if(zAcc>0){sign  = 1;}
+    else{sign  = -1;}
+    roll  = atan2( yAcc,sign* sqrt(zAcc*zAcc+ miu*xAcc*xAcc)) * 180.0/M_PI;
+    pitch  = atan2(-xAcc, sqrt(yAcc*yAcc + zAcc*zAcc)) * 180.0/M_PI;
+    
+    horzValue = map(roll, 30.000, -30.000, -0.006, 0.001);
+    vertValue = map(pitch, 30.000, -30.000, -0.006, 0.001);
 
-    // print the yaw, pitch and roll
-    roll = filter.getRoll();
-    pitch = filter.getPitch();
-    yaw = filter.getYaw();
-      } 
-    vertValue = yaw - vertZero; 
-    horzValue = roll - horzZero; 
-    vertZero = yaw; 
-    horzZero = roll;  
-    horzValue = atan2(yAcc, zAcc) * 180/M_PI;
-    vertValue = atan2(xAcc, sqrt(yGyro*yGyro + zGyro*zGyro)) * 180/M_PI;
-    horzValue = (horzValue+prev_horzValue)/2;
-    vertValue = (vertValue+prev_vertValue)/2;
+    Serial.print("Pitch: "+String(pitch));
+    Serial.print("      Roll:"+String(roll));
+    Serial.print("      horzValue: "+String(horzValue));
+    Serial.println("      vertValue:"+String(vertValue));
+    
     // Retrieve the HID service handle.
     auto *hid = mouse.hid();
 
     // Update internal values.
-//    float theta = PI * (roll / pitch);
-    hid->motion(horzValue * sensitivity, vertValue * sensitivity);
-    prev_horzValue = horzValue;
-    prev_vertValue = vertValue;
-    
+    hid->motion(roll *sensitivity , pitch *sensitivity);
     // Send them !
     hid->SendReport();
 }
